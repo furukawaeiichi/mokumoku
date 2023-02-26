56c46683b99ac5f5b04576c3ced25dc07b7c6a2e
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-02-26 14:07:34 +0900

    2/26

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_24_064732) do
+ActiveRecord::Schema.define(version: 2023_02_25_075715) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -136,7 +136,7 @@ ActiveRecord::Schema.define(version: 2023_02_24_064732) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "gender"
+    t.integer "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
